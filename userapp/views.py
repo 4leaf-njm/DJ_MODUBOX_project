6bbcdb324501aae6fa5802f27985502c04dc65d9
4leaf-njm <4leaf.njm@gmail.com>
2021-08-02 14:11:29 +0900

    modify

--- userapp/views.py
+++ userapp/views.py
@@ -32,32 +32,40 @@ class UserMypageView(CustomUpdateView) :
 class UserFindView(CustomTemplateView) :
     template_name = "userapp/find.html"
 
-    def get(self, request, *args, **kwargs):
-        ctx = {} 
-
-        return self.render_to_response(ctx)
-
     def post(self, request, *args, **kwargs):
         find_type = self.request.POST.get("find_type")
         name = self.request.POST.get("name")
         username = self.request.POST.get("username")
         email = self.request.POST.get("email")
 
+        
+
         if find_type == "1":
-            print ("111")
+            users = User.objects.all().filter(name=name, email=email)
+            
+            print(users)
+
+            if(len(users) > 0):
+                ctx = {
+                    'user': users[0]
+                }
+                print(ctx.user)
+                return redirect('/user/find_id_confirm', ctx)
+            
+
         
         elif find_type == "2":
-            
-            print ("222")
+            ctx = {
+                'find_type': find_type,
+                'name': name,
+                'username': username,
+                'email': email
+            }
+            return redirect('/user/find_id_confirm', ctx)
 
-        ctx = {
-            'find_type': find_type,
-            'name': name,
-            'username': username,
-            'email': email
-        }
+        
 
-        return redirect('/user/find_id_confirm', ctx)
+        
 
 class UserFindIdConfirmView(CustomTemplateView) :
     template_name = "userapp/find_id_confirm.html"
