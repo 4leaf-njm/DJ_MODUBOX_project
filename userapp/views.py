ce112deb211dea5545e2a1fb8f1f2c4bcc7902b1
4leaf-njm <4leaf.njm@gmail.com>
2021-08-02 13:11:25 +0900

    modify

--- userapp/views.py
+++ userapp/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from core.views import CustomCreateView, CustomUpdateView, CustomTemplateView
 from userapp.forms import CustomUserCreationForm
 from userapp.models import User
 from django.urls import reverse, reverse_lazy
 from userapp.decorators import account_ownwership_required
-
 
 has_ownwership = [
     login_required,
@@ -32,6 +31,33 @@ class UserMypageView(CustomUpdateView) :
 
 class UserFindView(CustomTemplateView) :
     template_name = "userapp/find.html"
+
+    def get(self, request, *args, **kwargs):
+        ctx = {} 
+
+        return self.render_to_response(ctx)
+
+    def post(self, request, *args, **kwargs):
+        find_type = self.request.POST.get("find_type")
+        name = self.request.POST.get("name")
+        username = self.request.POST.get("username")
+        email = self.request.POST.get("email")
+
+        if find_type == "1":
+            print ("111")
+        
+        elif find_type == "2":
+            
+            print ("222")
+
+        ctx = {
+            'find_type': find_type,
+            'name': name,
+            'username': username,
+            'email': email
+        }
+
+        return redirect('/user/find_id_confirm', ctx)
 
 class UserFindIdConfirmView(CustomTemplateView) :
     template_name = "userapp/find_id_confirm.html"
